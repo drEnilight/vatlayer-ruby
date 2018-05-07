84644b94c40abe537b9f9da7e56be3833e779e49
Vlad Romaniuk <romanuk_v@live.ru>
2018-05-07 15:10:28 +0300

    remove extra files

--- lib/vatlayer/ruby.rb
+++
@@ -1,7 +0,0 @@
-require "vatlayer/ruby/version"
-
-module Vatlayer
-  module Ruby
-    # Your code goes here...
-  end
-end
--- lib/vatlayer/ruby/version.rb
+++
@@ -1,5 +0,0 @@
-module Vatlayer
-  module Ruby
-    VERSION = "0.1.0"
-  end
-end
