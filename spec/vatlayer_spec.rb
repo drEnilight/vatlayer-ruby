9bd696421e66c4ebc28aef031bf7af49e509aece
Vlad Romaniuk <romanuk_v@live.ru>
2018-05-07 18:53:57 +0300

    add specs for vatlayer

--- spec/vatlayer_spec.rb
+++ spec/vatlayer_spec.rb
@@ -5,13 +5,31 @@ require 'securerandom'
 
 describe Vatlayer do
   describe '.configure' do
-    context 'with defined access_key' do
-      let(:secret_key) { SecureRandom.hex(10) }
-      before { described_class.configure { |config| config.access_key = secret_key } }
+    let(:configuration) { described_class.configuration }
 
-      it 'returns an array with 10 elements' do
-        expect(draw).to be_a(Array)
-        expect(draw.size).to eq(10)
+    before { Vatlayer.instance_variable_set :@configuration, nil }
+
+    context 'with access_key' do
+      before { described_class.configure { |config| config.access_key = 'a595997d18403d7e5022' } }
+
+      it 'returns object with configured access_key' do
+        expect(configuration.access_key).to eq('a595997d18403d7e5022')
+      end
+
+      it 'returns object with default https' do
+        expect(configuration.https).to be_falsey
+      end
+    end
+
+    context 'with https' do
+      before { described_class.configure { |config| config.https = true } }
+
+      it 'returns object with default access_key' do
+        expect(configuration.access_key).to eq('Your Access key')
+      end
+
+      it 'returns object with default https' do
+        is_expected.to be_truthy
       end
     end
   end
