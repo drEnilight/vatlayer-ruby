de1e6b5680ff5c393619861029bb246ea3414a50
Vlad Romaniuk <romanuk_v@live.ru>
2018-05-10 11:41:27 +0300

    replace getter on attr_reader

--- lib/vatlayer/response.rb
+++ lib/vatlayer/response.rb
@@ -2,15 +2,13 @@
 
 module Vatlayer
   class Response
+    attr_reader :error
+
     def initialize(attributes)
       attributes.each do |(key, value)|
         self.class.class_eval { attr_accessor :"#{key}" }
         public_send("#{key}=", key == 'error' ? value['type'] : value)
       end
-    end
-
-    def error
-      @error
     end
   end
 end
