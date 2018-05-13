b44fbef102268adf605ee4325f5852c2a23d3331
Vlad Romaniuk <romanuk_v@live.ru>
2018-05-13 18:19:39 +0300

    remove error test coz 2 times tested

--- spec/vatlayer/api/validate_spec.rb
+++ spec/vatlayer/api/validate_spec.rb
@@ -13,13 +13,8 @@ describe Vatlayer::Api do
     context 'with valid access_key' do
       let(:access_key) { '9deff624ebf413677b8599f7f6a3df00' }
       let(:result) { Vatlayer::Response::Data.new(response) }
-      let(:response) { Hash['valid', true,
-                            'database', 'ok',
-                            'format_valid', true,
-                            'query', 'LU26375245',
-                            'country_code', 'LU',
-                            'vat_number', '26375245',
-                            'company_name', 'AMAZON EUROPE CORE S.A R.L.',
+      let(:response) { Hash['valid', true, 'database', 'ok', 'format_valid', true, 'query', 'LU26375245',
+                            'country_code', 'LU', 'vat_number', '26375245', 'company_name', 'AMAZON EUROPE CORE S.A R.L.',
                             'company_address', '5, RUE PLAETIS L-2338  LUXEMBOURG'] }
 
       it 'returns correct data type' do
@@ -30,25 +25,6 @@ describe Vatlayer::Api do
         is_expected.to have_attributes(valid: true, database: 'ok', format_valid: true, query: 'LU26375245',
                                        country_code: 'LU', vat_number: '26375245', company_name: 'AMAZON EUROPE CORE S.A R.L.',
                                        company_address: '5, RUE PLAETIS L-2338  LUXEMBOURG')
-      end
-    end
-
-    context 'with invalid access_key' do
-      let(:access_key) { 'invalid_key' }
-      let(:result) { Vatlayer::Response::Data.new(response) }
-      let(:error) { Hash['code', 101,
-                         'type', 'invalid_access_key',
-                         'info', 'You have not supplied a valid API Access Key. [Technical Support: support@apilayer.com]' ] }
-      let(:response) { Hash['success', false,
-                          'error', error] }
-
-      it 'returns false status' do
-        is_expected.to have_attributes(success: false)
-      end
-
-      it 'returns error message' do
-        expect(subject.error).to have_attributes(code: 101, type: 'invalid_access_key',
-                                                 info: 'You have not supplied a valid API Access Key. [Technical Support: support@apilayer.com]')
       end
     end
   end
